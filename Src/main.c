dd648d41bfe0194ce49986b5cced66fcc6d147d3
JLouis-1987 <117316256+JLouis-1987@users.noreply.github.com>
2025-05-07 22:18:56 -0500

    Updating main.c to latest with working clock

--- Src/main.c
+++ Src/main.c
@@ -17,13 +17,23 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#include "stm32f4xx.h"
+#include "clock.h"
+#include "timer.h"
+#include "led.h"
+
+/*TODOs
+ * - Enable UARTs for CLI
+ * - Enable the CLI
+ * -
+ */
 
 int main(void)
 {
-    /* Loop forever */
-	for(;;);
+	clock_init();
+	init_leds();
+	tim4_output_compare();
+
 }
